f29f0adc9b6500214bf4221be3f4e2dd86f1a380
Diego Osvaldo <doctsystems@gmail.com>
2024-03-31 12:31:54 -0400

    verbos http
    Atributos para verbos HTTP

--- DemoPlatzi/Controllers/WeatherForecastController.cs
+++ DemoPlatzi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DemoPlatzi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -12,22 +12,42 @@ namespace DemoPlatzi.Controllers
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private static List<WeatherForecast> ListWeatherForecasts = new List<WeatherForecast>();
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
+            if (ListWeatherForecasts is null || !ListWeatherForecasts.Any())
+            {
+                ListWeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToList();
+            }
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return ListWeatherForecasts;
+        }
+
+        [HttpPost]
+        public IActionResult Post(WeatherForecast weatherForecast)
+        {
+            ListWeatherForecasts.Add(weatherForecast);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            ListWeatherForecasts.RemoveAt(index);
+            return Ok();
         }
     }
 }
