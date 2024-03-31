7ac79c59370b73f5d9ec3d828337129a4c756d81
Diego Osvaldo <doctsystems@gmail.com>
2024-03-31 12:33:22 -0400

    middlewares
    Creando un nuevo middleware

---
+++ DemoPlatzi/Middlewares/TimeMidleware.cs
@@ -0,0 +1,36 @@
+﻿namespace DemoPlatzi.Middlewares
+{
+    public class TimeMidleware
+    {
+        // nos permite invocar el middleware siguiente en el ciclo
+        readonly RequestDelegate next;
+
+        public TimeMidleware(RequestDelegate nextRequest)
+        {
+            next = nextRequest;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // invocamos al siguiente middleware
+            await next.Invoke(context);
+
+            // verificamos si en el Request (url) existe el parametro time
+            if (context.Request.Query.Any(p => p.Key == "time"))
+            {
+                // agregamos la hora actual a la respuesta del request
+                await context.Response.WriteAsync(DateTime.Now.ToShortTimeString());
+            }
+        }
+    }
+
+    // clase que nos permite invocar nuestro middleware desde la app
+    public static class TimeMiddlewareExtension
+    {
+        // recibimos el ApplicationBuilder y lo retornamos con nuetsro middleware agregado
+        public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TimeMidleware>();
+        }
+    }
+}
