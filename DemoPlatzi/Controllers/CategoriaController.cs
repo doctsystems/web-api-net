1cd3c445238c0ac6ffb85b47a2d51d0e75649a25
Diego Osvaldo <doctsystems@gmail.com>
2024-04-01 00:58:27 -0400

    add controllers
    Creando controladores

---
+++ DemoPlatzi/Controllers/CategoriaController.cs
@@ -0,0 +1,45 @@
+﻿using DemoPlatzi.Models;
+using DemoPlatzi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoPlatzi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriaController : ControllerBase
+    {
+        ICategoriaService categoriaService;
+
+        public CategoriaController(ICategoriaService categoriaService)
+        {
+            this.categoriaService = categoriaService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(categoriaService.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Categoria categoria)
+        {
+            categoriaService.Save(categoria);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Categoria categoria, Guid id)
+        {
+            categoriaService.Update(categoria, id);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            categoriaService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ DemoPlatzi/Controllers/TareaController.cs
@@ -0,0 +1,44 @@
+﻿using DemoPlatzi.Models;
+using DemoPlatzi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoPlatzi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TareaController : ControllerBase
+    {
+        ITareaService tareaService;
+
+        public TareaController(ITareaService tareaService)
+        {
+            this.tareaService = tareaService;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(tareaService.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Tarea tarea)
+        {
+            tareaService.Save(tarea);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Tarea tarea, Guid id)
+        {
+            tareaService.Update(tarea, id);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            tareaService.Delete(id);
+            return Ok();
+        }
+    }
+}
